a206aaef4a797e1e913a434a0ebbfc8f7666a929
raggiesoft <michael_ragsdale@hotmail.com>
2014-10-18 21:19:19 -0400

    Index.php
    Created the index.php file as a placeholder

---
+++ site/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TransitMix Presenter</title>
+	</head>
+	<body>
+		<p>This is my homepage for TransitMix Presenter.  Fill in data here.  Plan on adding backbone.js to make this document responsive</p>
+	</body>
+</html>
